7c70d7d388b2583a7997b1ccd3708b06e0b3b57f
JANG-MINWOO <alsdn2606@gmail.com>
2024-10-19 11:02:22 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 4.80 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+
+        Set<Integer> intSet = new HashSet<Integer>();
+        for(int i=0;i<numbers.length-1;i++){
+            for(int j=i+1;j<numbers.length;j++){
+                intSet.add(numbers[i]+numbers[j]);
+            }
+        }
+        int[] answer= intSet.stream().mapToInt(Integer::intValue).toArray();
+        Arrays.sort(answer);
+        return answer;
+    }
+}
