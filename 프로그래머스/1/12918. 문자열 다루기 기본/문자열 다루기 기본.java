a71457979877adf59717e8f5e5ebadf8eb851485
JANG-MINWOO <alsdn2606@gmail.com>
2024-10-19 11:14:56 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.11 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+        if(!s.matches("[0-9]+")||(s.length()!=4&&s.length()!=6))
+            answer = false;
+            
+        return answer;
+    }
+}
