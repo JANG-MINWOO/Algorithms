387301bf141fccf4213b6c15d863af68dd4e5bcc
JANG-MINWOO <alsdn2606@gmail.com>
2024-11-02 12:51:58 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 17.47 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,13 +1,15 @@
 import java.util.Arrays;
+
 class Solution {
     public String solution(String s) {
-        String answer = " ";
+        String result = " ";
         int[] numbers = Arrays.stream(s.split(" "))
             .mapToInt(Integer::parseInt)
             .toArray();
+        
         int min = Arrays.stream(numbers).min().getAsInt();
         int max = Arrays.stream(numbers).max().getAsInt();
         
-        return min+answer+max;
+        return min + result + max;
     }
 }
