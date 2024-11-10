8ed9da409f5303379eb1928cf8169b34daea363a
JANG-MINWOO <alsdn2606@gmail.com>
2024-11-10 12:02:22 +0900

    [level 1] Title: 덧칠하기, Time: 1.94 ms, Memory: 93.6 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,13 +1,14 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
-        int answer = 0;
+        int result = 0;
         int start = 0;
+        
         for(int i = 0 ; i < section.length ; i++){
             if(section[i]>start){
-                answer++;
+                result++;
                 start = section[i]+m-1;
             }
         }
-        return answer;
+        return result;
     }
 }
