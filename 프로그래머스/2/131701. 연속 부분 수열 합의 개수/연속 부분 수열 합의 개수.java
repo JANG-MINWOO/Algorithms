d141c9b487982b071a38f9ae9bee1d091d8b715e
Minwoo <alsdn2606@gmail.com>
2024-12-03 09:40:18 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 261.04 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -4,21 +4,21 @@ class Solution {
     public int solution(int[] elements) {
         int n = elements.length;
         Set<Integer> uniqueSums = new HashSet<>();
-        // 원형 수열 확장
+        
         int[] circular = new int[2 * n];
         for (int i = 0; i < n; i++) {
             circular[i] = circular[i + n] = elements[i];
-        }
-        // 슬라이딩 윈도우로 부분 합 계산
+        } // 원형 수열을 두배로 만들기
+
         for (int length = 1; length <= n; length++) { // 부분 수열 길이
             for (int start = 0; start < n; start++) { // 시작 인덱스
                 int sum = 0;
-                for (int k = 0; k < length; k++) { // 부분 수열 합 계산
+                for (int k = 0; k < length; k++) { // 부분 수열 합
                     sum += circular[start + k];
                 }
-                uniqueSums.add(sum); // 중복 제거하며 추가
+                uniqueSums.add(sum);
             }
         }
-        return uniqueSums.size(); // 고유한 합의 개수 반환
+        return uniqueSums.size();
     }
 }
