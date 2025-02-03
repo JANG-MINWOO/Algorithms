f33d4adaeaa266ccb281b4939dffa7f4628f89f5
Minwoo <alsdn2606@gmail.com>
2025-02-03 18:40:47 +0900

    [level 2] Title: 피로도, Time: 3.59 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,22 +1,22 @@
 import java.util.*;
 
 class Solution {
-    int maxCount = 0; // 탐험할 수 있는 최대 던전 개수
+    int maxCount = 0; //최대로 탐험할 수 있는 던전 개수
     
     public int solution(int k, int[][] dungeons) {
-        boolean[] visited = new boolean[dungeons.length]; // 방문 여부 배열
+        boolean[] visited = new boolean[dungeons.length];
         dfs(k, dungeons, visited, 0);
         return maxCount;
     }
     
     private void dfs(int k, int[][] dungeons, boolean[] visited, int count) {
-        maxCount = Math.max(maxCount, count); // 현재까지 탐험한 던전 개수 저장
-
+        maxCount = Math.max(maxCount, count);
+        
         for (int i = 0; i < dungeons.length; i++) {
-            if (!visited[i] && k >= dungeons[i][0]) { // 아직 방문하지 않았고, 최소 필요 피로도 만족
-                visited[i] = true; // 방문 표시
-                dfs(k - dungeons[i][1], dungeons, visited, count + 1); // 피로도 감소 후 다음 탐색
-                visited[i] = false; // 백트래킹 (원상복구)
+            if (!visited[i] && k >= dungeons[i][0]) {
+                visited[i] = true;
+                dfs(k-dungeons[i][1], dungeons, visited, count+1);
+                visited[i] = false;
             }
         }
     }
