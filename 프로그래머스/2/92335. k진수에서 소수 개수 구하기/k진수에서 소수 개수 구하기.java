8f37978217cb1f30a9b3e1ae7d4c425ea58d593e
Minwoo <alsdn2606@gmail.com>
2025-02-06 16:47:36 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 19.80 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,26 +1,31 @@
 class Solution {
     public int solution(int n, int k) {
-        // 1. 정수를 k진법으로 변환
-        String converted = Integer.toString(n, k);
-
-        // 2. '0'을 기준으로 숫자 나누기
-        String[] candidates = converted.split("0");
-
+        String converted = Integer.toString(n, k); // n을 k 진법으로 전환
+        String[] list = converted.split("0");
+        
         int count = 0;
-        for (String num : candidates) {
+        for (String num : list) {
             if (!num.isEmpty() && isPrime(Long.parseLong(num))) {
                 count++;
             }
         }
         return count;
     }
-
-    // 3. 소수 판별 함수
+    
     private boolean isPrime(long num) {
-        if (num < 2) return false; // 0, 1은 소수가 아님
+        if (num < 2) return false; // 0, 1 은 소수가 아니기 때문
         for (long i = 2; i * i <= num; i++) {
-            if (num % i == 0) return false; // 나누어떨어지면 소수가 아님
+            if (num % i == 0) return false;
         }
         return true;
     }
+    // num = 11 일 때
+    // i = 2, 3 까지만 가능
+    // i = 2 일 때, 11 % 2 = 1
+    // i = 3 일 때, 11 % 3 = 2
+    // true 반환
+    
+    // num = 10 일 때
+    // i = 2, 3 까지만 가능
+    // i = 2 일 때, 10 % 2 = 0, false 반환
 }
