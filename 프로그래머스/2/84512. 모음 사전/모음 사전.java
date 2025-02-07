9fac458a3231e9339735576636a81f079c606371
Minwoo <alsdn2606@gmail.com>
2025-02-07 18:01:27 +0900

    [level 2] Title: 모음 사전, Time: 0.20 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,20 +1,7 @@
 class Solution {
     public int solution(String word) {
-        int answer = 0;
-        
-        int[] multiplier = new int[6];
-        multiplier[5] = 1;
-        
-        for (int i = 4; i >= 0; i--) {
-            multiplier[i] = multiplier[i+1] * 5 + 1;
-        }
-        
-        String vowels = "AEIOU";
-        
-        for (int i = 0; i < word.length(); i++) {
-            int idx = vowels.indexOf(word.charAt(i));
-            answer += idx * multiplier[i+1] + 1;
-        }
+        int answer = 0, per = 3905;
+        for(String s : word.split("")) answer += "AEIOU".indexOf(s) * (per /= 5) + 1;
         return answer;
     }
 }
