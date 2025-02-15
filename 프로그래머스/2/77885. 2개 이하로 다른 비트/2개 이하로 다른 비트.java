09ce97d3b843503b8968ed942c51ea51ef93ad86
Minwoo <alsdn2606@gmail.com>
2025-02-15 18:11:08 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 17.84 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -1,10 +1,28 @@
 class Solution {
     public long[] solution(long[] numbers) {
-        long[] answer = numbers.clone();
-        for(int i = 0; i< answer.length; i++){
-            answer[i]++;
-            answer[i] += (answer[i]^numbers[i])>>>2;
+        long[] answer = new long[numbers.length];
+
+        for (int i=0;i<numbers.length;i++) {
+            long num = numbers[i];
+            long zero = 1;
+            long one = 0;
+
+            while (num > 0) {
+                //가장 오른쪽에 있는 0을 1로 바꿈
+                //0의 오른쪽에 1이 있는 경우 가장 가까운 1을 0으로 바꿈
+                if (num % 2 == 0) {
+                    break;
+                } else {
+                    zero *= 2;
+                    if (one == 0) one = 1;
+                    else one *= 2;
+                    num /= 2;
+                }
+            }
+
+            answer[i] = numbers[i] + zero - one;
         }
+
         return answer;
     }
 }
