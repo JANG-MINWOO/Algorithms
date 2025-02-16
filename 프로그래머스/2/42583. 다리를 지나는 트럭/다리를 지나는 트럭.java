df94af38e27192c20a45469211bb9c84d28bbda3
Minwoo <alsdn2606@gmail.com>
2025-02-16 19:39:44 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 36.19 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,34 +2,31 @@ import java.util.*;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        Queue<Integer> bridge = new LinkedList<>();
-        int time = 0;
-        int current_weight = 0;
+        int answer = 0;
         int index = 0;
+        int current_weight = 0;
+        
+        Queue<Integer> bridge = new LinkedList<>();
         
         for (int i = 0; i < bridge_length; i++) {
             bridge.offer(0);
         }
         
-        while (!bridge.isEmpty()) { // 모든 트럭이 다리를 건널 때까지 반복
-            time++;
-            
-            current_weight -= bridge.poll(); // 다리 건넌 트럭은 제거
-            
+        while (!bridge.isEmpty()) {
+            answer++;
+            current_weight -= bridge.poll(); // 가장 먼저들어간 차 나가고 그만큼 무게 빼기
             if (index < truck_weights.length) {
                 int nextTruck = truck_weights[index];
                 
-                // 새로운 트럭이 다리에 올라갈 수 있는지 확인
                 if (current_weight + nextTruck <= weight) {
                     bridge.offer(nextTruck);
-                    current_weight += nextTruck;
                     index++;
+                    current_weight += nextTruck;
                 } else {
                     bridge.offer(0);
                 }
             }
-        }
-        
-        return time;
+        } 
+        return answer;
     }
 }
