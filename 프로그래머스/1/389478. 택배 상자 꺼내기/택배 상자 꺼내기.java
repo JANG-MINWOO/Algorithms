b7d72fd3e648872e99b7c2676a3464c093606908
Minwoo <alsdn2606@gmail.com>
2025-03-02 15:54:19 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.15 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -3,25 +3,26 @@ import java.util.*;
 class Solution {
     public int solution(int n, int w, int num) {
         List<List<Integer>> columns = new ArrayList<>();
+        
         for (int i = 0; i < w; i++) {
             columns.add(new ArrayList<>());
-        }
+        } // w 의 맞춰서 세로열 리스트 설정
         
-        int boxNumber = 1;
         boolean leftToRight = true;
-
-        while (boxNumber <= n) {
+        int boxCount = 1;
+        
+        while (boxCount <= n) {
             if (leftToRight) {
-                for (int i = 0; i < w && boxNumber <= n; i++) {
-                    columns.get(i).add(boxNumber++);
+                for (int i = 0; i < w && boxCount <= n; i++) {
+                    columns.get(i).add(boxCount++);
                 }
             } else {
-                for (int i = w - 1; i >= 0 && boxNumber <= n; i--) {
-                    columns.get(i).add(boxNumber++);
+                for (int i = w - 1; i >= 0 && boxCount <= n; i--) {
+                    columns.get(i).add(boxCount++);
                 }
-            }
-            leftToRight = !leftToRight; // 방향 전환
-        }
+            } //한번 가로로 진행이 끝나고 나면
+            leftToRight = !leftToRight; // 방향전환
+        } // 지그재그로 박스 전부쌓기
         
         int count = 0;
         for (List<Integer> column : columns) {
@@ -30,7 +31,6 @@ class Solution {
                 break;
             }
         }
-        
         return count;
     }
 }
