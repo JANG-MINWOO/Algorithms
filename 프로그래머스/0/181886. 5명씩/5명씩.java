aa14a672ded77206255e0556f5108428e99ff944
Minwoo <alsdn2606@gmail.com>
2025-03-16 14:53:53 +0900

    [level 0] Title: 5명씩, Time: 0.03 ms, Memory: 84.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] names) {
+        List<String> nameList = new ArrayList<>();
+        for (int i = 0; i < names.length; i++) {
+            if (i % 5 == 0) {
+                nameList.add(names[i]);
+            }
+        }
+        String[] answer = new String[nameList.size()];
+        
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = nameList.get(i);
+        }
+        
+        return answer;
+    }
+}
