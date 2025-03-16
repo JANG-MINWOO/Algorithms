e7c283be7ffe9b3c83630e8e0f50fe0adc66b4fa
Minwoo <alsdn2606@gmail.com>
2025-03-16 17:36:05 +0900

    [level 0] Title: 0 떼기, Time: 6.64 ms, Memory: 70.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String n_str) {
+        int converted = Integer.parseInt(n_str);
+        
+        String answer = "" + converted;
+        return answer;
+    }
+}
