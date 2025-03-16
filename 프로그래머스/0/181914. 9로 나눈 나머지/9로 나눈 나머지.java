a57c2176fa7f512da1feaa82ff22af2646d6a7db
Minwoo <alsdn2606@gmail.com>
2025-03-16 16:51:35 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 56.39 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String number) {
+        int answer = 0;
+        int sum = 0;
+        String[] numArr = number.split("");
+        
+        for (int i = 0; i < numArr.length; i++) {
+            sum += Integer.parseInt(numArr[i]);
+        }
+        
+        answer = sum % 9;
+        return answer;
+    }
+}
