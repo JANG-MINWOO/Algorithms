93d6931a2d7f034bfb2e53efbee9a3bea6642bb2
Minwoo <alsdn2606@gmail.com>
2026-03-15 19:33:29 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.04 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -1,18 +1,11 @@
 class Solution {
     public int solution(String my_string, String is_prefix) {
         int answer = 0;
-        int length = is_prefix.length();
-        
-        if (length > my_string.length()) {
-            return answer;
+        String s = "";
+        if (is_prefix.length() <= my_string.length()) {
+            s = my_string.substring(0, is_prefix.length());
         }
-        
-        String substring = my_string.substring(0, length);
-        
-        if (substring.equals(is_prefix)) {
-            answer = 1;
-        }
-        
+        if (s.equals(is_prefix)) return 1;
         return answer;
     }
 }
