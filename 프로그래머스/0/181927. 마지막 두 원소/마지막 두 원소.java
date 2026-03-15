e786d6b426fc867861a7a04d973e308f19a905ea
Minwoo <alsdn2606@gmail.com>
2026-03-15 19:14:26 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -1,17 +1,15 @@
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[num_list.length + 1];
-        int length = num_list.length;
-        
         for (int i = 0; i < answer.length; i++) {
-            if (i == answer.length - 1) {
-                if (num_list[length - 2] < num_list[length - 1]) {
-                    answer[i] = num_list[length - 1] - num_list[length - 2];
-                } else {
-                    answer[i] = num_list[length - 1] * 2;
-                }
-            } else {
+            if (i != answer.length - 1) {
                 answer[i] = num_list[i];
+            } else {
+                if (num_list[num_list.length - 1] > num_list[num_list.length - 2]) {
+                    answer[i] = num_list[num_list.length - 1] - num_list[num_list.length - 2];
+                } else {
+                    answer[i] = num_list[num_list.length - 1] * 2;
+                }
             }
         }
         return answer;
