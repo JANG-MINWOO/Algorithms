d7e4722c81c21abcbdad66bc986421354fe093f5
Minwoo <alsdn2606@gmail.com>
2026-03-18 00:58:32 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 6.48 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -1,11 +1,6 @@
 class Solution {
     public int solution(String myString, String pat) {
-        int answer = 0;
-        myString = myString.toLowerCase();
-        pat = pat.toLowerCase();
-        
-        if (myString.contains(pat)) answer = 1;
-        else answer = 0;
-        return answer;
+        if(myString.toLowerCase().contains(pat.toLowerCase())) return 1;
+        return 0;
     }
 }
