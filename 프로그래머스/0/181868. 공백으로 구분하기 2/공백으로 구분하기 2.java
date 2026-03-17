9b59a039d840bf157b6f5abe59470139a8993289
Minwoo <alsdn2606@gmail.com>
2026-03-18 00:19:08 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 1.22 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -1,10 +1,5 @@
 class Solution {
     public String[] solution(String my_string) {
-        my_string = my_string.replaceAll("\\s+", " ");
-        if (my_string.indexOf(' ') == 0) {
-            my_string = my_string.substring(1, my_string.length());
-        }
-        String[] answer = my_string.split(" ");;
-        return answer;
+        return my_string.trim().split("\\s+");
     }
 }
