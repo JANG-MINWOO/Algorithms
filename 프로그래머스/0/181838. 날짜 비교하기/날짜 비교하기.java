286dffef73c7cd482280e84b880cd8b5b107e115
Minwoo <alsdn2606@gmail.com>
2026-03-19 00:41:17 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.03 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -1,10 +1,8 @@
 class Solution {
     public int solution(int[] date1, int[] date2) {
-        int answer = 0;
-        int check = (date2[0] - date1[0]) * 365 + (date2[1] - date1[1]) * 30 + (date2[2] - date1[2]) * 1;
-        if (check >= 1) {
-            answer = 1;
-        }
-        return answer;
+        int d1 = date1[0] * 10000 + date1[1] * 100 + date1[2];
+        int d2 = date2[0] * 10000 + date2[1] * 100 + date2[2]; 
+        
+        return d1 < d2 ? 1 : 0;
     }
 }
