ab468f9632bd94f1e27a789f473e56c3f928d742
Minwoo <alsdn2606@gmail.com>
2026-03-18 23:41:44 +0900

    [level 0] Title: 수 조작하기 2, Time: 10.33 ms, Memory: 94.1 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,17 +1,13 @@
 class Solution {
     public String solution(int[] numLog) {
-        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        
         for (int i = 0; i < numLog.length - 1; i++) {
-            if ((numLog[i + 1] - numLog[i]) == 1) {
-                answer += "w";
-            } else if ((numLog[i + 1] - numLog[i]) == -1) {
-                answer += "s";
-            } else if ((numLog[i + 1] - numLog[i]) == 10) {
-                answer += "d";
-            } else {
-                answer += "a";
-            }
+            if (numLog[i + 1] - numLog[i] == 1) sb.append("w");
+            else if (numLog[i + 1] - numLog[i] == -1) sb.append("s");
+            else if (numLog[i + 1] - numLog[i] == 10) sb.append("d");
+            else sb.append("a");
         }
-        return answer;
+        return sb.toString();
     }
 }
