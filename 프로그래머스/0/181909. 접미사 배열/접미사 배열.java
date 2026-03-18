d4e77c746cb469f48dac17164b88ab3ec92bc65d
Minwoo <alsdn2606@gmail.com>
2026-03-18 23:35:34 +0900

    [level 0] Title: 접미사 배열, Time: 1.04 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public String[] solution(String my_string) {
@@ -7,9 +7,7 @@ class Solution {
         for (int i = 0; i < answer.length; i++) {
             answer[i] = my_string.substring(i);
         }
-        
         Arrays.sort(answer);
-        
         return answer;
     }
 }
