59e5afb17959bdb1960532265c8308bab6d20e4c
Minwoo <alsdn2606@gmail.com>
2026-03-18 22:49:03 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.06 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -1,15 +1,19 @@
+import java.util.List;
+import java.util.ArrayList;
+
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        int[] answer = new int[num_list.length];
-        int length = num_list.length;
+        List<Integer> list = new ArrayList<>();
         
-        for (int i = 0; i < length; i++) {
-            if ((i + n) >= length) {
-                answer[i] = num_list[i + n - length];
-            } else {
-                answer[i] = num_list[i + n];
-            }
-            
+        for (int i = n; i < num_list.length; i++) {
+            list.add(num_list[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            list.add(num_list[i]);
+        }
+        int[] answer = new int[num_list.length];
+        for (int i = 0; i < num_list.length; i++) {
+            answer[i] = list.get(i);
         }
         return answer;
     }
