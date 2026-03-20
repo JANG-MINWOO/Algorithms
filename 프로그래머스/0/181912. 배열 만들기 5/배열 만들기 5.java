9ab05cc814eb4c8485b3119ed826f442bd82a915
Minwoo <alsdn2606@gmail.com>
2026-03-20 18:22:17 +0900

    [level 0] Title: 배열 만들기 5, Time: 9.80 ms, Memory: 92.1 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -1,33 +1,14 @@
-import java.util.*;
+import java.util.List;
+import java.util.ArrayList;
 
 class Solution {
     public int[] solution(String[] intStrs, int k, int s, int l) {
-        List<Integer> result = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         
         for (int i = 0; i < intStrs.length; i++) {
-            List<String> list = new ArrayList<>();
-            String[] splited = intStrs[i].split("");
-            
-            for (int j = s; j < s + l; j++) {
-                list.add(splited[j]);
-            }
-            String check = "";
-            for (int j = 0; j < list.size(); j++) {
-                check += list.get(j);
-            }
-            
-            int convert = Integer.parseInt(check);
-            
-            if (convert > k) {
-                result.add(convert);
-            }
+            int a = Integer.parseInt(intStrs[i].substring(s ,s + l));
+            if (a > k) list.add(a);
         }
-        
-        int[] answer = new int[result.size()];
-        
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = result.get(i);
-        }
-        return answer;
+        return list.stream().mapToInt(i -> i).toArray();
     }
 }
