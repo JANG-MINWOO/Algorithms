4d38847952df5d8bce0b7c5441196cd681d5801f
Minwoo <alsdn2606@gmail.com>
2026-03-20 18:36:14 +0900

    [level 0] Title: 1로 만들기, Time: 0.03 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -1,14 +1,21 @@
 class Solution {
     public int solution(int[] num_list) {
         int answer = 0;
-        for (int i : num_list) {
-            while (i > 1) {
-                answer++;
-                if (i % 2 == 0) {
-                    i /= 2;
+        for (int i = 0; i < num_list.length; i++) {
+            int count = 0;
+            int check = num_list[i];
+            if (check == 1) continue;
+            while(true) {
+                if (check % 2 == 0) {
+                    check /= 2;
+                    count++;
                 } else {
-                    i --;
-                    i /= 2;
+                    check = (check - 1) / 2;
+                    count++;
+                }
+                if (check == 1) {
+                    answer += count;
+                    break;
                 }
             }
         }
