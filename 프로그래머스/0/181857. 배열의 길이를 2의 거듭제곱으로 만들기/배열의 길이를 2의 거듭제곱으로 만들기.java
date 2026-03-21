d13e784d1aa8c589e03fcd933483aec4e14c3450
Minwoo <alsdn2606@gmail.com>
2026-03-21 17:10:55 +0900

    [level 0] Title: 배열의 길이를 2의 거듭제곱으로 만들기, Time: 3.19 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/0/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.java
+++ 프로그래머스/0/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.java
@@ -1,45 +1,34 @@
-import java.util.*;
+import java.util.List;
+import java.util.ArrayList;
 
 class Solution {
     public int[] solution(int[] arr) {
         int length = arr.length;
         int check = 1;
-        int theLengthIsUnderThan = 0;
-        boolean pass = false;
+        int count = 0;
         
-        if (length == 1) {
-            pass = true;
-            return arr;
-        }
-        
-        while (check < 1024) {
+        while(true) {
+            if (check <= length && check * 2 > length) break;
             check *= 2;
-            if (length == check) {
-                pass = true;
-                break;
-            } else if (length < check) {
-                theLengthIsUnderThan = check;
-                break;
-            }
-        }
-        if (pass) {
-            return arr;
+            count++;
+        } // 4 ->  1 <= a < 2 false -> check 2, count 1 -> 2 <= 4 < 4 false -> check 4, count 2 -> 4 <= 4 < 8
+        if (count == 0) return arr; 
+        
+        int count2 = 0;
+        
+        for (int i = 0; i < count; i++) {
+            count2 = check * 2 - length;
         }
         List<Integer> list = new ArrayList<>();
         
-        for (int i : arr) {
+        for (int i: arr) {
             list.add(i);
         }
-        
-        for (int i = 0; i < theLengthIsUnderThan - length; i++) {
+        for (int i = 0; i < count2; i++) {
+            if (check == length) break;
             list.add(0);
         }
-        int[] answer = new int[list.size()];
         
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
-        }
-        
-        return answer;
+        return list.stream().mapToInt(i -> i).toArray();
     }
 }
