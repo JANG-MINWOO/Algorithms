551ac86ff8b40d6369f1f0b755c0377ee2f4903b
Minwoo <alsdn2606@gmail.com>
2026-03-29 17:05:02 +0900

    [level 3] Title: 네트워크, Time: 0.17 ms, Memory: 85.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,24 @@
+class Solution {
+    boolean[] visited;
+    public int solution(int n, int[][] computers) {
+        visited = new boolean[n];
+        int count = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i, computers, n);
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    void dfs(int node, int[][] computers, int n) {
+        visited[node] = true;
+        for (int j = 0; j < n; j++) {
+            if (computers[node][j] == 1 && !visited[j]) {
+                dfs(j, computers, n);
+            }
+        }
+    }
+}
