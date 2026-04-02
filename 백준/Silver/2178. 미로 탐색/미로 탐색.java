68d4e04e89576fedaa5eb80c18a4a3fb598dcd43
Minwoo <alsdn2606@gmail.com>
2026-04-02 23:34:52 +0900

    [Silver I] Title: 미로 탐색, Time: 108 ms, Memory: 14604 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -18,6 +18,7 @@ public class Main {
                 map[i][j] = line.charAt(j) - '0';
             }
         }
+        
         int[] dx = {1, -1, 0, 0};
         int[] dy = {0, 0, 1, -1};
         
